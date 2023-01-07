cbbd5a39743341cd100fcf62180e2983997d9a13
NagajyothiChinnem <chinnemnagajyothi123@gmail.com>
2023-01-07 10:45:39 +0530

    added details

---
+++ details.java
@@ -0,0 +1,9 @@
+class details{
+public static void main(String args[]){
+System.out.println("Rollnumber:215n1f0066");
+System.out.println("name:jyothi");
+System.out.println("course:MCA");
+System.out.println("aadhar:92692");
+System.out.println("village:B.T.padu");
+}
+}
